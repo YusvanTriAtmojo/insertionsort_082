5154ad7dbe4012affa08e632e6fec9c907898836
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2023-03-24 15:07:32 +0700

    Menambahkan perulangan while saat memasukkan data array

--- InsertionSort/InsertionSort.cpp
+++ InsertionSort/InsertionSort.cpp
@@ -5,4 +5,16 @@ int arr[20];
 int n;
 
 void input() {
+	while (true) {
+		cout << "masukkan jumlah data pada array : ";
+		cin >> n;
+
+		if (n <= 20) {
+			break;
+		}
+		else
+		{
+			cout << "\nArray yang anda masukkan maksimal 20 elemen.\n";
+		}
+	}
 	
